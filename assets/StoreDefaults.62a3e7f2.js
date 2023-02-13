be64d764a9aa18979e46cd0f044fd79d5f013825
tachidesk-vui <n/a>
2023-02-13 19:22:46 +0000

    Update from https://github.com/Suwayomi/Tachidesk-VUI/commit/2f38802e639695917a99a27662d3457a8591242b

---
+++ assets/StoreDefaults.62a3e7f2.js
@@ -0,0 +1,1 @@
+import{P as t}from"./index.16a21bb1.js";t.has("ExtLangFilt")||t.set("ExtLangFilt",["en"]);
---
+++ assets/use-quasar.2f573033.js
@@ -0,0 +1,1 @@
+import{i as a,a1 as r}from"./index.16a21bb1.js";function u(){return a(r)}export{u};
